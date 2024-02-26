3bf9e1117b3ba54fd68d9f75760f39e056b8ea4b
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2024-02-26 11:56:45 +0200

    added authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
-'use server'; import { sql } from '@vercel/postgres';
+'use server';
+import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from "@/auth"
+import { AuthError } from 'next-auth'
 import { z } from 'zod';
 
 export type State = {
@@ -98,4 +101,23 @@ export async function deleteInvoice(id: string) {
     }
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
